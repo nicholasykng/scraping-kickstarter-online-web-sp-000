21f51c617385d8d2d5249c91dc0e91bf771eba7c
Nicholas Ng <nicholasykng@gmail.com>
2019-11-03 23:24:19 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,11 +4,12 @@ require "pry"
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  projects = {}
 end
 create_project_hash
 #kickstarter.css("li.project.grid_4").first
 #project.css("h2.bbcard_name strong a").text
 #project.css("div.project-thumbnail a img").attribute("src").value
 #project.css("p.bbcard_blurb").text
-project.css("span.location_name").text
+#project.css("ul.project-meta span.location-name").text
+#project.css("ul.project-stats li.first.funded strong").text
