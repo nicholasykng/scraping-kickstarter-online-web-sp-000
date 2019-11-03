4fcff08d6584fb4268c197520bbdd1239e33bfea
Nicholas Ng <nicholasykng@gmail.com>
2019-11-03 23:12:31 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,3 +10,4 @@ create_project_hash
 #kickstarter.css("li.project.grid_4").first
 #project.css("h2.bbcard_name strong a").text
 #project.css("div.project-thumbnail a img").attribute("src").value
+#project.css("p.bbcard_blurb").text
